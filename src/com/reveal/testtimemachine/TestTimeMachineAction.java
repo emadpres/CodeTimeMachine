2707360f9c4611eb6b44bf7670674ce9e7e4a93c
emadpres <emadpres@gmail.com>
2016-11-23 17:33:48 +0100

    Making an empty Action
    Making an empty Action.

---
+++ src/com/reveal/testtimemachine/TestTimeMachineAction.java
@@ -0,0 +1,17 @@
+package com.reveal.testtimemachine;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by emadpres on 11/23/16.
+ */
+public class TestTimeMachineAction extends AnAction
+{
+
+    @Override
+    public void actionPerformed(AnActionEvent e)
+    {
+        // TODO: insert action logic here
+    }
+}
