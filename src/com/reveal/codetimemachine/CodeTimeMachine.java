92f89bc2dcdf45fbc69e701978466246eefcb7da
emadpres <emadpres@gmail.com>
2017-01-25 14:19:23 +0100

    Tool Window Icon

--- src/com/reveal/codetimemachine/CodeTimeMachine.java
+++ src/com/reveal/codetimemachine/CodeTimeMachine.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ToolWindowType;
+import javax.swing.*;
 
 public class CodeTimeMachine
 {
@@ -15,8 +17,13 @@ public class CodeTimeMachine
     public CodeTimeMachine(Project project)
     {
         this.project = project;
-        toolWindow = ToolWindowManager.getInstance(project).registerToolWindow("Code Time Machine      ", false, ToolWindowAnchor.RIGHT);
+
         gitHelper = new GitHelper(project);
+
+        toolWindow = ToolWindowManager.getInstance(project).registerToolWindow("Code Time Machine ", true/*Can close tabs?*/, ToolWindowAnchor.RIGHT);
+        toolWindow.setType(ToolWindowType.WINDOWED,null); // make it window, and not docked or anything else
+        ImageIcon icon = new ImageIcon(getClass().getResource("/images/appIcon.png"));
+        toolWindow.setIcon(icon);
     }
 
     public ToolWindow getToolWindow()
